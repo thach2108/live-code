a4fe8df5060e76529cc24b35bf2f5248498d42ce
Thach. Do Dang - CMC Global DE1 <ddthach@cmcglobal.vn>
2022-05-18 15:39:40 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import ProductLists from "./views/ProductLists";
+import ProductDetail from "./views/ProductDetail";
+import About from "./views/About";
+import Navbar from "./components/Navbar";
+import 'antd/dist/antd.css';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/products" element={<ProductLists />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div>
+      <header className='header'>Our Home Website</header>
+    </div>
+  );
+};
+
+export default Home;
